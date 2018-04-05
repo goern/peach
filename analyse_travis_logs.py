d781c0afe970a875b4276508dcd981bef4125178
Christoph Görn <goern@redhat.com>
2018-04-05 15:49:30 +0200

    minor tweaks

--- analyse_travis_logs.py
+++ analyse_travis_logs.py
@@ -25,26 +25,41 @@ import re
 import csv
 
 import luigi
-
+from luigi.contrib.spark import PySparkTask
 
 __version__ = '0.1.0'
 
 
-class ProcessTravisJob(luigi.Task):
+class ProcessTravisJob(PySparkTask):
     owner = luigi.Parameter()
     job_id = luigi.Parameter()
 
     def output(self):
-        return luigi.LocalTarget('data/{}/jobs/{}/summary.txt'.format(self.owner, self.job_id))
+        return luigi.LocalTarget('data/{}/jobs/{}/summary.json'.format(self.owner, self.job_id))
+
+    def main(self, sc, * args):
+        pass
 
     def run(self):
+        result = {}  # This is our analyser result
+        result['succeeded'] = True
+        result['owner_slug'] = self.owner
+        result['job_id'] = self.job_id
+
         ANSI_ESCAPE = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
         with open(
                 'data/{}/jobs/{}/log.txt'.format(self.owner, self.job_id), 'r') as infile, self.output().open('w') as outfile:
 
             for line in infile:
-                outfile.write(ANSI_ESCAPE.sub('', str(line)))
+                # strip all Ascii ESC seqs
+                line = ANSI_ESCAPE.sub('', str(line))
+
+                if line.startswith('Done. Your build exited with'):
+                    if '0.' not in line.split(' ')[-1]:
+                        result['succeeded'] = False
+
+            json.dump(result, outfile)
 
 
 class ProcessTravisJobs(luigi.Task):
